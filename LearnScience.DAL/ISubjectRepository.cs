5453b84d18950564b1fcff8a7d9c4c2297c159b2
wengst <wengst@126.com>
2022-12-07 12:05:42 +0800

    2022.12.7 12:04更新

--- LearnScience.DAL/ISubjectRepository.cs
+++ LearnScience.DAL/ISubjectRepository.cs
@@ -1,7 +1,6 @@
 ﻿using LS.Core;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LS.DAL
 {
@@ -10,6 +9,7 @@ namespace LS.DAL
     /// </summary>
     interface ISubjectRepository
     {
+        #region 待审试题操作
         /// <summary>
         /// 保存试题
         /// <para>无论是保存编辑过的旧试题还是保存新试题，首先都要进入审核库，只有审核过之后，才能进入试题的发布库</para>
@@ -41,7 +41,22 @@ namespace LS.DAL
         /// <param name="pageIndex"></param>
         /// <param name="pageSize"></param>
         /// <returns></returns>
-        IEnumerable<SubjectReView> GetSubjectReViews(int pageIndex=D.Default_IndexPage,int pageSize=D.Default_PageSize);
+        IEnumerable<SubjectReView> GetSubjectReViews(int pageIndex = D.Default_IndexPage, int pageSize = D.Default_PageSize);
+
+        /// <summary>
+        /// 根据待审试题Id获取问题列表
+        /// </summary>
+        /// <param name="sujectId"></param>
+        /// <returns></returns>
+        IEnumerable<QuestionReView> GetQuestionBySubjectReView(Guid sujectId);
+
+        /// <summary>
+        /// 根据待审试题组Id获取试题
+        /// <para>待审试题组中的试题来自于审核过的试题</para>
+        /// </summary>
+        /// <param name="testPaperId"></param>
+        /// <returns></returns>
+        IEnumerable<Subject> GetSubjectsByTestPaperReView(Guid testPaperId);
 
         /// <summary>
         /// 审核试题
@@ -52,15 +67,6 @@ namespace LS.DAL
         /// <param name="reason">如果未通过，需要提供原因</param>
         /// <returns></returns>
         bool ReViewSubject(SubjectReView subject, bool isAccept, string reason = null);
-
-        /// <summary>
-        /// 上架或下架试题
-        /// <para>如果试题处于上架状态，执行此方法将下架试题，反之就是上架</para>
-        /// </summary>
-        /// <param name="authId">作者Id</param>
-        /// <param name="subjectId">试题Id</param>
-        /// <returns></returns>
-        bool PutonOrTakeOffSubject(Guid authId, Guid subjectId);
 
         /// <summary>
         /// 保存新或正在编辑的试题组
@@ -95,6 +101,26 @@ namespace LS.DAL
         /// <param name="reason">未通过审核的原因</param>
         /// <returns></returns>
         bool ReViewTestPaper(TestPaperReView testPaper, bool isAccept, string reason = null);
+        #endregion
+
+        #region 已审试题操作
+        /// <summary>
+        /// 上架或下架试题
+        /// <para>如果试题处于上架状态，执行此方法将下架试题，反之就是上架</para>
+        /// </summary>
+        /// <param name="authId">作者Id</param>
+        /// <param name="subjectId">试题Id</param>
+        /// <returns></returns>
+        bool PutonOrTakeOffSubject(Guid authId, Guid subjectId);
+
+        /// <summary>
+        /// 克隆试题
+        /// <para>克隆试题的目的是为了创建相似题，比如修改下其中的一些数据后保存到表中，成为相似题</para>
+        /// <para>新克隆出的试题处于草稿状态</para>
+        /// </summary>
+        /// <param name="subjectId"></param>
+        /// <returns></returns>
+        SubjectReView CloneSubject(Guid subjectId);
 
         /// <summary>
         /// 上架或下架试题组
@@ -104,5 +130,44 @@ namespace LS.DAL
         /// <param name="testPaperId">试题组Id</param>
         /// <returns></returns>
         bool PutonOrTakeOffTestPaper(Guid authId, Guid testPaperId);
+
+        /// <summary>
+        /// 分页获取试题组列表
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        IEnumerable<TestPaper> GetTestPapers(Guid? authId = null, int pageIndex = D.Default_IndexPage, int pageSize = D.Default_PageSize);
+
+        /// <summary>
+        /// 根据试题组Id获取试题组
+        /// </summary>
+        /// <param name="testPaperId"></param>
+        /// <returns></returns>
+        TestPaper GetTestPaper(Guid testPaperId);
+
+        /// <summary>
+        /// 克隆试题组
+        /// <para>克隆后的试题组，需要被克隆的试题组有不同但相似的试题组合</para>
+        /// <para>克隆出的新试题组处于草稿状态</para>
+        /// </summary>
+        /// <param name="testPaperId"></param>
+        /// <returns></returns>
+        TestPaperReView CloneTestPaper(Guid testPaperId);
+
+        /// <summary>
+        /// 获取试题组的试题列表
+        /// </summary>
+        /// <param name="testPaperId"></param>
+        /// <returns></returns>
+        IEnumerable<Subject> GetSubjectsByTestPaper(Guid testPaperId);
+
+        /// <summary>
+        /// 根据试题Id获取问题列表
+        /// </summary>
+        /// <param name="subjectId"></param>
+        /// <returns></returns>
+        IEnumerable<Question> GetQuestionsBySubject(Guid subjectId); 
+        #endregion
     }
 }
